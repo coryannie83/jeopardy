c18470c37e5a14964f97240d4f7ceefb4a3005a3
Cory Budday <cory.budday@libertymutual.com>
2017-06-18 11:24:55 -0400

    reworked code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,7 +5,6 @@
     let category = $('#category'); //category of question
     let value = $('#value');
     let question = $('#question');
-    //let answer = $('#answer');
     let guess = $('#guessinput');
     let finding = $('#finding');
     let submitButton = $('#submit');
@@ -13,34 +12,18 @@
     let score = $('#score');
 
     //---------on screen load-------------//
-    //pull data back
+
+    //declare result as a variable
+
     let result = $.get("http://jservice.io/api/random");
 
     //---???? how can i streamline and consolidate results??
     result.done(function(data) {
       $(category).html(data[0].category.title);
-        //console.log(data);
-    });
-
-    //dollar value
-    result.done(function(data) {
       $(value).html(data[0].value);
-    //  console.log(data);
-    });
-
-    //question
-    result.done(function(data) {
       $(question).html(data[0].question);
-      //console.log(data);
-    });
-
-    //answer
-    //need to hide until user clicks submit button
-    result.done(function(data) {
-      //   $(answer).html(data[0].answer);
-      hiddenAnswer = data[0].answer;
+       hiddenAnswer = data[0].answer;
       console.log(data);
-
     });
 
 
@@ -48,20 +31,18 @@
 
     //check user's answer
     submitButton.click(function() {
-
+      //system determines if user's answer matches answer from api call
       if (hiddenAnswer == guess.val()) {
-        finding.html("Correct!");
+        finding.html("Correct!"); //ui displays result
+        //add current score to correct question value to display new score
         var newScore = parseInt(value.html()) + parseInt(score.html());
         score.html(newScore);
-
       } else {
-        finding.html("Incorrect");
+        finding.html("Incorrect"); //ui displays result and current score is unchanged
       }
     });
 
     //-------reset screen - clear input and load new question----//
-
-    //--???  this is clumsy, how can i streamline?--//
 
     let resetButton = $('#reset');
     resetButton.click(function() {
@@ -71,27 +52,15 @@
 
       let newResult = $.get("http://jservice.io/api/random");
 
+
       newResult.done(function(data) {
         $(category).html(data[0].category.title);
-      });
-
-      newResult.done(function(data) {
         $(value).html(data[0].value);
-      });
-
-      newResult.done(function(data) {
         $(question).html(data[0].question);
-      });
-
-      newResult.done(function(data) {
-        //   $(answer).html(data[0].answer);
-        hiddenAnswer = data[0].answer;
+        $(question).html(data[0].question);
         console.log(data);
       });
-
-    });
-
   });
-
+  });
 
 })();
